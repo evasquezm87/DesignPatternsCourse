3c9de38b3b1529dbef470e5cee7963d58a347950
lvasquez <lvasquez@santos.com.mx>
2023-03-28 09:53:11 -0600

    Se añade patron Injection Dependency con ASP.Net

--- DesingPatterns/DesignPatternsASP/Controllers/ProductDetailController.cs
+++ DesingPatterns/DesignPatternsASP/Controllers/ProductDetailController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using System.Xml.Schema;
 using Tools.Earn;
 
@@ -6,18 +7,32 @@ namespace DesignPatternsASP.Controllers
 {
     public class ProductDetailController : Controller
     {
-        public IActionResult Index(decimal total)
+        //Para mandar el objeto que se creara desde la inyeccion de dependencias en Start.cs
+        private EarnFactory _localEarnFactory;
+        private ForeignEarnFactory _foreignEarnFactory;
+
+		//Se crea el constructor para que devuelva el objeto creado
+		public ProductDetailController(LocalEarnFactory localEarnFactory, ForeignEarnFactory foreignEarnFactory) { 
+            _localEarnFactory= localEarnFactory;
+			_foreignEarnFactory = foreignEarnFactory; ;
+		}
+
+		public IActionResult Index(decimal total)
         {
             //Crear la fabrica para crear el producto
             //Fabrica
-            LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.2m);
+            //Para el patro inyeccion de dependeicias se quita la responsabilidad a esta clase de crear el objeto, 
+            //Se manda llamar en Start.cs, donde se creara el objeto
+            //LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.2m);
+            
             //Crear los productos creados por la fabrica
             //Se crea un objeto del tipo localEarnFactory, el cual ya ejecuta los metodos que tiene           
-            var localEarn = localEarnFactory.GetEarn();
+            var localEarn = _localEarnFactory.GetEarn();
 
 			//Producto 2
-			ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.3m,10);
-			var foreignEarn = foreignEarnFactory.GetEarn();
+			//Se manda llamar en Start.cs, donde se creara el objeto
+			//ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.3m,10);
+			var foreignEarn = _foreignEarnFactory.GetEarn();
 
 			//Metodo para calcular las ganancias, el cual esta en el producto
 			ViewBag.totalLocal = total + localEarn.Earn(total);
--- DesingPatterns/DesignPatternsASP/Startup.cs
+++ DesingPatterns/DesignPatternsASP/Startup.cs
@@ -3,15 +3,23 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;//Para la inyeccion de dependencias
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tools.Earn;
 
 namespace DesignPatternsASP
 {
+	//Esta clase es la que usa .Net para el manejo de la inyeccion de dependencias
+	/*
+	 Se pueden inyectar objetos de 3 tipos
+	Singleton.- solo un objeto en la aplicaicon
+	Transitor.- un objeto para cada servicio, por cada controlador
+	Alcance(Scope).- Un objeto por la misma solicitud, a diferencia del anterior hay uno por cada controlador
+	 */
 	public class Startup
 	{
 		public Startup(IConfiguration configuration)
@@ -28,6 +36,22 @@ namespace DesignPatternsASP
 			//Inyección de dependencias, obtener valores del config para ser utilizado en el proyecto
 			//Inyeccción de un objeto para poder ser utilizado en cualquier controlador
 			services.Configure<MyConfig>(Configuration.GetSection("MyConfig"));
+			services.AddTransient((factory) =>
+			{
+				//En lugar de poner directamente el parametro en el codigo, llamarlo desde el archivo de configuración
+				//return new LocalEarnFactory(.20m);
+				return new LocalEarnFactory(Configuration.GetSection("MyConfig").GetValue<decimal>("LocalPercentage"));
+			}
+			);//Para crear inyeccion del tipo Transient aqui se crea el objeto
+			
+			//Objeto 2 creado para la inyeccion de dependencias
+			services.AddTransient((factory) =>
+			{
+				return new ForeignEarnFactory(Configuration.GetSection("MyConfig")
+					.GetValue<decimal>("ForeignPercentage"), Configuration.GetSection("MyConfig")
+					.GetValue<decimal>("Extra"));
+			}
+			);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
